1e4727a0beb4657a79403016c78e5f1d88f5014c
ByteBlazzerr <141044080+ByteBlazzerr@users.noreply.github.com>
2025-05-04 02:35:04 +0530

    commited backend

---
+++ Db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+import { DB_NAME } from "../constants.js"
+
+
+dotenv.config()
+const connectDB = async()=>{
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`Mongoose is connected :${DB_NAME}`);
+    } catch (error) {
+        console.log("mongoDB connection error :"+ error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
